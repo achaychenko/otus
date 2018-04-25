5f654913f578ba6c38cb299391fc880c8cac1b9d
achaychenko <alexchaychenko@gmail.com>
2018-04-25 22:13:10 +0300

    added hw2

---
+++ L021/src/main/java/С.java
@@ -0,0 +1,10 @@
+import ru.otus.achaychenko.ObjectSizeFetcher;
+
+public class C {
+    private int x;
+    private int y;
+
+    public static void main(String [] args) {
+        System.out.println(ObjectSizeFetcher.getObjectSize(new C()));
+    }
+}
