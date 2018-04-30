9333b328ba271be8ccef7ef804b8eaf67693b281
achaychenko <alexchaychenko@gmail.com>
2018-04-30 21:50:11 +0300

    Third HW

---
+++ L2.1HW3/src/main/java/ru/otus/achaychenko/stringTutor/Main.java
@@ -0,0 +1,9 @@
+package ru.otus.achaychenko.stringTutor;
+
+public class Main {
+    public static void main(String[] args) {
+        StringTutor st = new StringTutor();
+//        System.out.println(st.checkGreeting("Привет, Петр!"));
+        st.testCheckGreeting();
+    }
+}
