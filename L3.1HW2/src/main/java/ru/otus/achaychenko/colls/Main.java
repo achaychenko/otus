10edfb18250cb814f37ab64cdda449006b462266
achaychenko <alexchaychenko@gmail.com>
2018-05-20 23:50:05 +0300

    hw3

--- L3.1HW2/src/main/java/ru/otus/achaychenko/colls/Main.java
+++ L3.1HW2/src/main/java/ru/otus/achaychenko/colls/Main.java
@@ -1,7 +1,10 @@
 package ru.otus.achaychenko.colls;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.Collections;
+import java.util.Iterator;
+
+import static javafx.scene.input.KeyCode.T;
 
 public class Main {
 
@@ -9,19 +12,26 @@ public class Main {
 //        MyGeneric<Integer> intObj = new MyGeneric<>(1);
 //
 //        System.out.println(intObj.getValue());
-        MyArrayList<Integer> myAL = new MyArrayList<>(0);
+        MyArrayList<Integer> myAL = new MyArrayList<>();
+        MyArrayList<Integer> myAL2 = new MyArrayList<>();
         myAL.add(1);
         myAL.add(2);
-        Collections.addAll(myAL, 1, 2, 4, 5);
-        System.out.println(myAL.size());
+        System.out.println(myAL.set(1, 44));
+//        Collections.addAll(myAL, 1, 2, 4, 5);
+//
+//
+//        Collections.copy(myAL2, myAL);
 
-//        MyArrayList<Integer> myAl2 = new MyArrayList<>(10);
+
+//        MyArrayList<Integer> myAl2 = new MyArrayList<>();
 //        Collections.copy(myAl2, myAL);
-//        System.out.println(myAl2.size());
+        System.out.println(myAL.toString());
 
-        Collections.sort(myAL, new MyComparator<>());
-        System.out.println(Arrays.toString(myAL.toArray()));
+//        Collections.sort(myAL);
+//        System.out.println(Arrays.toString(myAL.toArray()));
 
-
+//        addAll(Collection<? super T> c, T... elements)
+//        static <T> void copy(List<? super T> dest, List<? extends T> src)
+//        static <T> void sort(List<T> list, Comparator<? super T> c)
     }
 }
--- L3.1HW2/src/main/java/ru/otus/achaychenko/colls/MyArrayLIst.java
+++ L3.1HW2/src/main/java/ru/otus/achaychenko/colls/MyArrayLIst.java
@@ -2,20 +2,23 @@ package ru.otus.achaychenko.colls;
 
 import java.util.*;
 
-class MyArrayList<T> implements List<T>, Iterable<T>, Comparable<T>{
-    private Object[] arr;
+class MyArrayList<T> implements List<T>, Comparable<T>, Iterable<T>{
+
+    private Object elements[];
     private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+    private int index;
     private int pos = 0;
 
-    MyArrayList(int sze){
-        if(sze > 0){
-            size = sze;
-        }else {
-            size = 0;
-        }
+    public MyArrayList(){
 
-        arr = new Object[10];
+        elements = new Object[DEFAULT_CAPACITY];
+        this.size = DEFAULT_CAPACITY;
+    }
 
+    @Override
+    public int compareTo(T o) {
+        return 0;
     }
 
     @Override
@@ -34,31 +37,8 @@ class MyArrayList<T> implements List<T>, Iterable<T>, Comparable<T>{
     }
 
     @Override
-    public Iterator<T> iterator() {
-        return new Iterator<>() {
-            private Object[] currentData = arr;
-            private int pos = 0;
-
-            @Override
-            public boolean hasNext() {
-                return pos < currentData.length;
-            }
-
-            @Override
-            public T next() {
-                return (T) currentData[pos++];
-            }
-
-            @Override
-            public void remove() {
-                MyArrayList.this.remove(pos++);
-            }
-        };
-    }
-
-    @Override
     public Object[] toArray() {
-        return Arrays.copyOf(arr, size);
+        return new Object[0];
     }
 
     @Override
@@ -66,20 +46,47 @@ class MyArrayList<T> implements List<T>, Iterable<T>, Comparable<T>{
         return null;
     }
 
+
+    @Override
+    public void add(int index, T element) {
+        if (index < 0 || index > this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+        // add the element to get the resizing
+        add(element);
+
+        // shift the elements
+        for (int i=this.size-1; i>index; i--) {
+            elements[i] = elements[i-1];
+        }
+        // put the new one in the right place
+        elements[index] = element;
+    }
+
     @Override
     public boolean add(T t) {
-        if (size >= arr.length) {
-            // make a bigger array and copy over the elements
-            @SuppressWarnings("unchecked")
-            T[] bigger = (T[]) new Object[arr.length * 2];
-            System.arraycopy(arr, 0, bigger, 0, arr.length);
-            arr = bigger;
+        if (this.index == this.size -1) {
+            increaseSizeAndReallocate();
+            return true;
         }
-        arr[size] = t;
-        size++;
+        elements[this.index] = t;
+        this.index++;
         return true;
     }
 
+    private void increaseSizeAndReallocate() {
+        this.size += DEFAULT_CAPACITY;
+        Object newElements[] = new Object[this.size];
+        for (int i = 0; i < elements.length; i++) {
+            newElements[i] = elements[i];
+        }
+        this.elements = newElements;
+    }
+
+    @Override
+    public String toString(){
+        return Arrays.toString(elements);
+    }
     @Override
     public boolean remove(Object o) {
         return false;
@@ -87,6 +94,7 @@ class MyArrayList<T> implements List<T>, Iterable<T>, Comparable<T>{
 
     @Override
     public boolean containsAll(Collection<?> c) {
+
         return false;
     }
 
@@ -120,34 +128,41 @@ class MyArrayList<T> implements List<T>, Iterable<T>, Comparable<T>{
     }
 
     @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            private Object[] currentData = elements;
+            private int pos = 0;
+
+            @Override
+            public boolean hasNext() {
+                return pos < currentData.length;
+            }
+
+            @Override
+            public T next() {
+                return (T) elements[pos++];
+            }
+
+            @Override
+            public void remove() {
+                MyArrayList.this.remove(pos++);
+            }
+        };
+    }
+
+    @Override
     public T get(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
-        return (T) arr[index];
+        return (T) elements[index];
     }
 
     @Override
     public T set(int index, T element) {
         T old = get(index);
-        arr[index] = element;
+        elements[index] = element;
         return old;
-    }
-
-    @Override
-    public void add(int index, T element) {
-        if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException();
-        }
-        // add the element to get the resizing
-        add(element);
-
-        // shift the elements
-        for (int i=size-1; i>index; i--) {
-            arr[i] = arr[i-1];
-        }
-        // put the new one in the right place
-        arr[index] = element;
     }
 
     @Override
@@ -178,10 +193,5 @@ class MyArrayList<T> implements List<T>, Iterable<T>, Comparable<T>{
     @Override
     public List<T> subList(int fromIndex, int toIndex) {
         return null;
-    }
-
-    @Override
-    public int compareTo(T o) {
-        return 0;
     }
 }
